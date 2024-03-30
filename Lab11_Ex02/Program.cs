fbebda8ce4b5188b6023e483002447490e7f0d05
newyork <spidermanwin1@gmail.com>
2024-03-31 02:09:34 +0700

    ex2

---
+++ Lab11_Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine(Calculator.AddValues(2, 3));
+System.Console.WriteLine(Calculator.AddValues(2.0f, 3.0f));
+System.Console.WriteLine(Calculator.AddValues(2L, 3L));
+
+public static class Calculator
+{
+    public static int AddValues(int a, int b)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int a, int b)");
+        return a + b;
+    }
+    
+    public static float AddValues(float f, float g)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(int f, int g)");
+        return (long) (f + g);
+    }
+   public static long AddValues(long h, long i)
+    {
+        System.Console.WriteLine("Calculated by method: public static long AddValues(long h, long i)");
+        return (long) (h + i);
+    }
+}
