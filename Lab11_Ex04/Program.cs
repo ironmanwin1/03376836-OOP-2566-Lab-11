1c3dbf674dc1aabf5ccd927d508a3eb33d729b8d
newyork <spidermanwin1@gmail.com>
2024-03-31 02:14:11 +0700

    ex4

---
+++ Lab11_Ex04/Program.cs
@@ -0,0 +1,29 @@
+﻿var b = new Base();
+var d1 = new Derived_1();
+var d2 = new Derived_2();
+
+b.A();
+d1.A();
+d2.A();
+
+class Base
+{
+    public virtual void A()
+    {
+        System.Console.WriteLine("Base.A()");
+    }
+}
+class Derived_1 : Base
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_1.A()");
+    }
+}
+class Derived_2 : Derived_1
+{
+    public override void A()
+    {
+        System.Console.WriteLine("Derived_2.A()");
+    }
+}
