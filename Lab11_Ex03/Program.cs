676bf46309ebcf1581c7b1b90f56587ae5ae0d82
newyork <spidermanwin1@gmail.com>
2024-03-31 02:12:08 +0700

    ex3

---
+++ Lab11_Ex03/Program.cs
@@ -0,0 +1,41 @@
+﻿var animalA = new Dog();
+var animalB = new Fish();
+var animalC = new Bird();
+
+animalA.Move();
+animalB.Move();
+animalC.Move();
+
+class Animal
+{
+   public virtual void Move()
+   {
+    System.Console.WriteLine("Move successfully.");
+   } 
+}
+
+class Dog:Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine($"{this.GetType()}: Running on the ground.");
+        base.Move();
+    }
+}
+class Fish: Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine($"{this.GetType()}: Swimming in the water.");
+        base.Move();
+    }
+}
+
+class Bird: Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine($"{this.GetType()}: Flying in the air.");
+        base.Move();
+    }
+}
